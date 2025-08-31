a59a1531367a91e021f01f7652c76046e80f6950
bizoonydb <166882000+bizoonydb@users.noreply.github.com>
2025-08-31 01:54:03 -0300

    Create service-worker.js

---
+++ cifra/service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'chord-keeper-cache-v1';
+const urlsToCache = [
+  './chord_keeper.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
